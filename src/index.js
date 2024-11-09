9b429ae8fc175f8bcb36ec95635d9815ebe77cd2
balembajessy <balembajessy@gmail.com>
2024-11-09 03:49:23 +0100

    Customising Expense-Manager USING Rollup bundler

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './HelloWorld';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <HelloWorld />
+    </React.StrictMode>,
+
+    document.getElementById('root')
+);
